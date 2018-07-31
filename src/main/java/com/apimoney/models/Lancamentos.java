d0187415c366fdb270bee1dc4747f210abda7c92
Luan <almeida.luan099@gmail.com>
2018-07-30 22:41:24 -0300

    Criação da classe Lancamento,
    TipoLancamento e script Flyway para
    
    criação da tabela no banco de dados

--- src/main/java/com/apimoney/models/Lancamentos.java
+++
@@ -1,8 +0,0 @@
-package com.apimoney.models;
-
-import javax.persistence.Entity;
-
-@Entity
-public class Lancamentos {
-
-}
---
+++ src/main/java/com/apimoney/models/TipoLancamento.java
@@ -0,0 +1,5 @@
+package com.apimoney.models;
+
+public enum TipoLancamento {
+	RECEITA, DESPESA
+}
