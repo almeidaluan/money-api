8cfca52d5383d6e4a6fb412c899eb7a804b0e5b0
lnascimento <lnascimento@NB8046.OGMASTER.LOCAL>
2018-08-08 16:56:13 -0300

    Implementação de testes no controller

--- src/test/java/com/apimoney/CategoriaTest.java
+++ src/test/java/com/apimoney/CategoriaTest.java
@@ -3,15 +3,11 @@ package com.apimoney;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import java.io.IOException;
-import java.nio.charset.Charset;
-import org.springframework.web.context.WebApplicationContext;
-import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
-import org.hamcrest.core.Is;
-import org.junit.Before;
+import java.nio.charset.Charset;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -19,12 +15,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.mock.http.MockHttpOutputMessage;
+
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
-import com.apimoney.models.Categoria;
+
+
 import com.apimoney.resource.CategoriaResource;
 
 /**
@@ -43,15 +39,30 @@ public class CategoriaTest {
 			MediaType.APPLICATION_JSON.getSubtype(), Charset.forName("utf8"));
 	
 	@Autowired
+	private MockMvc mockMvc;
+	
 	private CategoriaResource resource;
 
-	@Autowired
-	private MockMvc mockMvc;
-
 	  @Test
-	    public void methodCreateShouldReturnStatus400() throws Exception{
+	   void methodCreateShouldReturnStatus400() throws Exception{
 	        mockMvc.perform(post("/categorias")
 	                .contentType(contentType))
 	            .andExpect(status().isBadRequest());
 	    }	
+	  @Test
+	  void methodFindAllShouldReturnAllCategories() throws Exception {
+		  
+		  
+		  mockMvc.perform(get("/categorias")
+				  .contentType(MediaType.APPLICATION_JSON))
+		  		.andExpect(status().isOk());
+		  
+	  }
+	  
+	  @Test
+	  void methodDelete() throws Exception {
+		  mockMvc.perform(delete("/categorias/"+"123")).andExpect(status().isUnauthorized());
+	
+	  }
+	  
 }
